1af18e296a27ec33fab5e1f92d4527bed492643f
Sergi0let <s.vashkevych@gmail.com>
2022-06-17 17:20:53 +0300

    unit

--- sub-menu/js/style.js
+++ sub-menu/js/style.js
@@ -1,4 +1,5 @@
-let isMobile = {
+// провіряє чи сайт був відкритий на точ-скринні
+const isMobile = {
   Android: function () {
     return navigator.userAgent.match(/Android/i);
   },
@@ -26,20 +27,29 @@ let isMobile = {
 };
 
 const body = document.querySelector('body');
-if (isMobile.any()) {
-  body.classList.add('_touch');
-  const arrows = document.querySelectorAll('.arrow');
 
+if (isMobile.any()) {
+  // 1. Було відкрито на точСкріні то добаляємо класс _touch для body
+  body.classList.add('_touch');
+  // 3. Створюємо колекцію усіх стрілочок .arrow
+  const arrows = document.querySelectorAll('.arrow');
+  // 4. Проходимося циклом по стрілках
   for (const arrow of arrows) {
+    // 5. thisLink це елемент головних заголовків
     const thisLink = arrow.previousElementSibling;
+    // 6. subMenu це саме півменню
     const subMenu = arrow.nextElementSibling;
     const thisArrow = arrow;
+    // parent додаємо клас через який в CSs ми робимо відступ від стрілочки коли вона зявится та точСкрінні
     thisLink.classList.add('parent');
     arrow.addEventListener('click', function () {
+      // для відкриття півменню
       subMenu.classList.toggle('_open');
-      thisArrow.classList.toggle('_active');
+      // активна стрілка для перевороту через css
+      thisArrow.classList.toggle('_activeArrow');
     });
   }
 } else {
+  // 2. Я на компютері то класс _pc.
   body.classList.add('_pc');
 }
